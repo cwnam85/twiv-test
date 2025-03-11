9e649b4bfe7854a92855e81ab8e2b2fcaea401b1
SSA <158470963+shinseongan@users.noreply.github.com>
2025-03-11 11:51:23 +0900

    Feat: 페이지 임의용

--- src/app.js
+++ src/app.js
@@ -1,9 +1,18 @@
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express();
 const port = 3333;
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../front/index.html'));
+});
 
 app.listen(port, () => {
   console.log(`서버가 http://localhost:${port}에서 실행 중입니다.`);
