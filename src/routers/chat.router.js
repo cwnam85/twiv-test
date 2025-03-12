aeff7feca17f4b309a1773baf9a1a05166d0b84d
SSA <158470963+shinseongan@users.noreply.github.com>
2025-03-12 11:00:47 +0900

    Fix: router 기능 분리 및 tts 추가

--- src/routers/chat.router.js
+++ src/routers/chat.router.js
@@ -1,11 +1,9 @@
 import express from 'express';
-import { OpenAI } from 'openai';
 import fs from 'fs';
 import path from 'path';
-// import dotenv from 'dotenv';
 import { fileURLToPath } from 'url';
-
-// dotenv.config(); // 환경 변수 로드
+import { getLLMResponse } from '../services/llmService.js';
+import { playTTSSupertone } from '../services/ttsService.js';
 
 const router = express.Router();
 
@@ -13,14 +11,8 @@ const router = express.Router();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Grok API 초기화
-const grokApiKey = process.env.GROK_API_KEY;
-const grokClient = new OpenAI({
-  apiKey: grokApiKey,
-  baseURL: 'https://api.x.ai/v1',
-});
-
-let grokConversationHistory = []; // 대화 기록을 저장할 배열
+// 대화 기록을 저장할 배열
+let grokConversationHistory = [];
 
 // 시스템 지시사항 로드
 let systemInstruction = null;
@@ -51,34 +43,27 @@ router.post('/chat', async (req, res) => {
   grokConversationHistory.push(userMessageObj);
 
   try {
-    // Grok API에 메시지 전달
-    const completion = await grokClient.chat.completions.create({
-      model: 'grok-2-1212',
-      messages: grokConversationHistory,
-    });
-
-    if (completion && completion.choices && completion.choices.length > 0) {
-      let responseLLM = completion.choices[0].message.content;
-      responseLLM = responseLLM.replace(/\[TTS\]:/g, '').trim();
-      // 포즈 변경 여부로 과금 판단(임시)
-      const poseChangeMatch = responseLLM.match(/\[Pose\]:\s*(.*)/);
-      const isPaid = !!poseChangeMatch;
-      if (poseChangeMatch) {
-        responseLLM = responseLLM.replace(poseChangeMatch[0], '').trim();
-      }
-
-      // LLM 응답 메시지를 대화 기록에 추가
-      const assistantMessage = {
-        role: 'assistant',
-        content: responseLLM,
-      };
-      grokConversationHistory.push(assistantMessage);
-
-      // 클라이언트에 응답 전송
-      res.json({ message: responseLLM, isPaid });
-    } else {
-      res.status(500).json({ error: 'Grok API 응답이 유효하지 않습니다.' });
+    let responseLLM = await getLLMResponse(grokConversationHistory);
+    responseLLM = responseLLM.replace(/\[TTS\]:/g, '').trim();
+    // 포즈 변경 여부로 과금 판단(임시)
+    const poseChangeMatch = responseLLM.match(/\[Pose\]:\s*(.*)/);
+    const isPaid = !!poseChangeMatch;
+    if (poseChangeMatch) {
+      responseLLM = responseLLM.replace(poseChangeMatch[0], '').trim();
     }
+
+    // LLM 응답 메시지를 대화 기록에 추가
+    const assistantMessage = {
+      role: 'assistant',
+      content: responseLLM,
+    };
+    grokConversationHistory.push(assistantMessage);
+
+    // TTS 호출
+    await playTTSSupertone(responseLLM);
+
+    // 클라이언트에 응답 전송
+    res.json({ message: responseLLM, isPaid });
   } catch (error) {
     console.error('Error calling Grok API:', error);
     res.status(500).json({ error: 'Grok API 호출 중 오류가 발생했습니다.' });
