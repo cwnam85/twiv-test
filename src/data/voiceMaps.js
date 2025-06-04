4384808a89b041757f60920f58ddcc41299b9584
DevIruka <qkralswns1998@gmail.com>
2025-06-04 17:31:34 +0900

    feat : tts 교체 기능 추가.

---
+++ src/data/voiceMaps.js
@@ -0,0 +1,24 @@
+export const characterVoiceMaps = {
+  shaki: {
+    neutral: '1ZGUVjuoNk52se64NfSpVN',
+    happy: 'fzMCAQiQ5ttUSAvbFoco6a',
+    funny: '5cwZd6ujcYQKLx9fG3ahH7',
+    affectionate: 'jM5CATR5x6TgJx5JWUt5HV',
+    annoyed: '3WecMGvANcdunskVJNZyez',
+    sad: '1ZGUVjuoNk52se64NfSpVN',
+    embarrassed: 'ob4wAp1heaDzEqDepEpJ5Z',
+    dominating: 'axwTSPmp7hCxS4r5HmV8aP',
+    aroused: '4YzPr4g1EKLp2HDiZdPgDA',
+  },
+  meuaeng: {
+    happy: 'c8H8iXDrSPGaVA57SWktBC',
+    dominating: 'azEEyrEy3h6GpciFN6rsce',
+    embarrassed: 'oLk1DUnm1eGS2vRTjtF6ha',
+    sad: 'v7TkG21gSiKqbHjGRZGCSe',
+    affectionate: '15SmdvmcA1sfn1MEP3ECB9',
+    funny: 'qKYFukMosCo9SKziQ3cmMD',
+    neutral: '7N2viC2v73zF68RDa9kpZh',
+    annoyed: '7qckEbw5UBrsnR8DSp8V7v',
+    angry: 'azEEyrEy3h6GpciFN6rsce',
+  },
+};
